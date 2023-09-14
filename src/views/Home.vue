5c59df810d22eb6e8b8a0c67fafd61c84dff5fed
muhammad bilal <bilal.kazmi@tw1.com>
2023-09-14 17:01:47 +0500

    first commit added vuetify, tailwind and removed boilerplate code

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="text-lg bg-red-200">ref</div>
+    <v-btn prepend-icon="$vuetify" stacked> Button </v-btn>
+  </main>
+</template>
